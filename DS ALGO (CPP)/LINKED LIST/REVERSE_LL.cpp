c06ac81ea6886d5eec6311e079e146cd6a1a3b61
DIVYANSH GARG <dishugarg1511@gmail.com>
2021-05-01 15:45:23 +0530

    DAY 5

---
+++ DS ALGO (CPP)/LINKED LIST/REVERSE_LL.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int data){
+        this->data = data;
+        next = NULL;
+    }
+};
+
+node* takeinput(){
+    node* head = NULL;
+    node* tail = NULL;
+
+    int data;
+    cin>>data;
+
+    while (data != -1)
+    {
+        node* newnode = new node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = newnode;
+        }
+        cin>>data;
+    }
+    return head;
+}
+
+void print(node* head){
+    while (head != NULL)
+    {
+        cout<<head->data<<endl;
+        head = head->next;
+    }
+}
+
+node* reverse(node* head){
+    node* prev = NULL;
+    node* curr = head;
+    node* temp = NULL;
+
+    while (curr != NULL)
+    {
+        temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    return prev;
+}
+
+int main(){
+    node* head = takeinput();
+    print(head);
+    node* reve = reverse(head);
+    print(reve);
+}

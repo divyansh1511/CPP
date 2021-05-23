f4ae7a83f33e5050ba944adcf825805bba59f099
DIVYANSH GARG <dishugarg1511@gmail.com>
2021-05-23 21:40:56 +0530

    DAY 27

--- DS ALGO (CPP)/Dynamic Programing/edit_distance.cpp
+++ DS ALGO (CPP)/Dynamic Programing/edit_distance.cpp
@@ -3,21 +3,21 @@
 #include<iostream>
 using namespace std;
 
-string editdistancedp(string s1 ,string s2){
+int editdistancedp(string s1 ,string s2){
     int m = s1.length();
     int n = s2.length();
-    string arr[m+1][n+1];
+    int arr[m+1][n+1];
     for (int i = 0; i < m+1; i++)
     {
-        arr[i][0] = "";
+        arr[i][0] = i;
     }
     for (int i = 0; i < n+1; i++)
     {
-        arr[0][i] = "";
+        arr[0][i] = i;
     }
-    for (int i = 0; i < m+1; i++)
+    for (int i = 1; i < m+1; i++)
     {
-        for (int j = 0; j < n+1; j++)
+        for (int j = 1; j < n+1; j++)
         {
             if (s1[m-i] == s2[n-j])
             {
@@ -25,7 +25,7 @@ string editdistancedp(string s1 ,string s2){
             }
             else
             {
-                arr[i][j] = "1"+min(min(arr[i-1][j] , arr[i][j-1]) , arr[i-1][j-1]);
+                arr[i][j] = 1+min(min(arr[i-1][j] , arr[i][j-1]) , arr[i-1][j-1]);
             }
         }
     }

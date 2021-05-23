04167fcec8083fe0f8dcb223d24d49085eb42b31
DIVYANSH GARG <dishugarg1511@gmail.com>
2021-05-23 11:44:10 +0530

    DAY 27

---
+++ DS ALGO (CPP)/Dynamic Programing/LCS.cpp
@@ -0,0 +1,66 @@
+//-----------NOT COMPLLETED------------
+
+
+#include<iostream>
+using namespace std;
+
+string LCSrecursion(string s1 , string s2){
+    if (s1.length() == 0 || s2.length() == 0)
+    {
+        return "";
+    }
+    
+    if (s1[0] == s2[0])
+    {
+        return s1[0]+LCSrecursion(s1.substr(1),s2.substr(1));
+    }
+    else
+    {
+        string option1 = LCSrecursion(s1 , s2.substr(1));
+        string option2 = LCSrecursion(s1.substr(1) , s1);
+        if (option1.length() > option2.length())
+        {
+            return option1;
+        }
+        else
+        {
+            return option2;
+        }
+    }
+}
+
+string LCSdp(string s1 ,string s2){
+    int m = s1.length();
+    int n = s2.length();
+    string arr[m+1][n+1];
+    for (int i = 0; i < m+1; i++)
+    {
+        arr[i][0] = "";
+    }
+    for (int i = 0; i < n+1; i++)
+    {
+        arr[0][i] = "";
+    }
+    for (int i = 0; i < m+1; i++)
+    {
+        for (int j = 0; j < n+1; j++)
+        {
+            if (s1[m-i] == s2[n-j])
+            {
+                arr[i][j] = s1[m-i]+arr[i-1][j-1];
+            }
+            else
+            {
+                string option1 = arr[i-1][j];
+                string option2 = arr[i][j-1];
+                arr[i][j] = (option1.length() > option2.length())?option1:option2;
+            }
+        }
+    }
+    return arr[m][n];
+}
+
+int main(){
+    //cout<<LCSrecursion("abcdefghi","bghicfhi")<<endl;
+    cout<<LCSdp("abcdefghi","bghicfhi")<<endl;
+}
---
+++ DS ALGO (CPP)/Dynamic Programing/edit_distance.cpp
@@ -0,0 +1,37 @@
+//------NOT COMPLTED-----
+
+#include<iostream>
+using namespace std;
+
+string editdistancedp(string s1 ,string s2){
+    int m = s1.length();
+    int n = s2.length();
+    string arr[m+1][n+1];
+    for (int i = 0; i < m+1; i++)
+    {
+        arr[i][0] = "";
+    }
+    for (int i = 0; i < n+1; i++)
+    {
+        arr[0][i] = "";
+    }
+    for (int i = 0; i < m+1; i++)
+    {
+        for (int j = 0; j < n+1; j++)
+        {
+            if (s1[m-i] == s2[n-j])
+            {
+                arr[i][j] = arr[i-1][j-1];
+            }
+            else
+            {
+                arr[i][j] = "1"+min(min(arr[i-1][j] , arr[i][j-1]) , arr[i-1][j-1]);
+            }
+        }
+    }
+    return arr[m][n];
+}
+
+int main(){
+    cout<<editdistancedp("abcd","ecd")<<endl;
+}

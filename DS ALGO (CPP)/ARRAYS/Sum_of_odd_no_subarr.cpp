7e4a5dd4e39a3bd1e55d6bcdbc765f198e20f1b4
DIVYANSH GARG <dishugarg1511@gmail.com>
2021-05-24 23:33:44 +0530

    DAY 28

---
+++ DS ALGO (CPP)/ARRAYS/Sum_of_odd_no_subarr.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int sumofaodd(vector<int> arr){
+    int ans = 0;
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        int contribution = ceil((i+1)*(n-i)/2.0);
+        ans += (contribution*arr[i]);
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr;
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin>>a;
+        arr.push_back(a);
+    }
+    cout<<sumofaodd(arr)<<endl;
+}

f350a60fa22091ae0941d8094b5dfbba7521e503
DIVYANSH GARG <dishugarg1511@gmail.com>
2021-08-19 23:40:18 +0530

    dp

---
+++ DS ALGO (CPP)/Dynamic Programing/easy/goldmine.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int goldmine(int arr[][4] , int n , int m){
+    int goldtable[n][m];
+    memset(goldtable , 0 , sizeof(goldtable));
+
+    for (int col = m-1; col >= 0; col--)
+    {
+        for (int row = 0; row < n; row++)
+        {
+            int right = (col == n-1) ? 0 : goldtable[row][col+1];
+
+            int right_up = (row == 0 || col == n-1) ? 0 : goldtable[row-1][col+1];
+
+            int right_down = (row == m-1 || col == n-1) ? 0 : goldtable[row+1][col+1];
+
+            goldtable[row][col] = arr[row][col] + max({right , right_down , right_up});
+        }
+    }
+    
+    int res = goldtable[0][0];
+    for (int i = 0; i < n; i++)
+    {
+        res = max(res , goldtable[i][0]);
+    }
+    return res;
+}
+
+int main(){
+    int gold[4][4]= { {1, 3, 1, 5},
+        {2, 2, 4, 1},
+        {5, 0, 2, 3},
+        {0, 6, 1, 2}
+    };
+    int m = 4, n = 4;
+    cout << goldmine(gold, m, n);
+}

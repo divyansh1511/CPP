e67623a1f88a4f1fbb13ba3c3706aefec23e1e59
DIVYANSH GARG <dishugarg1511@gmail.com>
2021-09-07 17:57:12 +0530

    m

--- DS ALGO (CPP)/GREEDY/max_arr_sum.cpp
+++ DS ALGO (CPP)/GREEDY/max_arr_sum.cpp
@@ -1,31 +1,34 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int solve(int arr[] , int n , int k){
-    sort(arr, arr + n);
-    int i=0;
-    int sum = 0;
-    while (k > 0)
-    {
-        if (arr[i] >= 0)
-        {
-            k = 0;
-        }
-        else
-        {
-            arr[i] = arr[i]*(-1);
-            k--;
-        }
-        i++;
-    }
-    for(int j = 0; j < n; j++)
-    {
-        sum += arr[j];
-    }
-    return sum;
+int maximumSum(int arr[], int n, int k)
+{
+	for (int i = 1; i <= k; i++)
+	{
+		int min = INT_MAX;
+		int index = -1;
+		for (int j = 0; j < n; j++)
+		{
+			if (arr[j] < min) {
+				min = arr[j];
+				index = j;
+			}
+		}
+		if (min == 0)
+			break;
+		arr[index] = -arr[index];
+	}
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+		sum += arr[i];
+	return sum;
 }
 
-int main(){
-    
+int main()
+{
+	int arr[] = { -2, 0, 5, -1, 2 };
+	int k = 4;
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << maximumSum(arr, n, k);
+	return 0;
 }

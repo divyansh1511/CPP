baf32a6edbdefc0158db7b094f80ece273e02396
DIVYANSH GARG <dishugarg1511@gmail.com>
2021-10-21 18:45:14 +0530

    modi

--- DS ALGO (CPP)/Dynamic Programing/0-1knapsack.cpp
+++ DS ALGO (CPP)/Dynamic Programing/0-1knapsack.cpp
@@ -37,19 +37,22 @@ int knapsacmem(int wt[] , int val[] , int W , int n){
 }
 
 int knapsactab(int wt[] , int val[] , int W , int n){
-    int dp[W+1];
-    memset(dp , 0 , sizeof(dp));
-    for (int i = 1; i < n+1; i++)
+    int dp[n+1][W+1];
+    for(int i = 0; i <= n; i++)
     {
-        for (int j = W; j >= 0; j--)
+        for(int w = 0; w <= W; w++)
         {
-            if (wt[i-1] <= j)
-            {
-                dp[j] = max(val[i-1] + dp[j - wt[i-1]] , dp[j]);
-            }
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                dp[i][w] = max(val[i - 1] +
+                                dp[i - 1][w - wt[i - 1]],
+                                dp[i - 1][w]);
+            else
+                dp[i][w] = dp[i - 1][w];
         }
     }
-    return dp[W];
+    return dp[n][W];
 }
 
 int main(){

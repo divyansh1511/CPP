912b0d95701499fcee436a77e6f607e526d1b8d3
DIVYANSH GARG <dishugarg1511@gmail.com>
2021-10-26 16:34:09 +0530

    babbar

---
+++ DS ALGO (CPP)/ARRAYS/count_say.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+string countandsay(int n){
+    if (n == 1)
+    {
+        return "1";
+    }
+    if (n == 2)
+    {
+        return "11";
+    }
+    string s = "11";
+    for (int i = 3; i <= n; i++)
+    {
+        string t = "";
+        s = s+"&";
+        int c = 1;
+        for (int j = 1; j < s.length(); j++)
+        {
+            if (s[j] != s[j-1])
+            {
+                t += to_string(c);
+                t += s[j-1];
+                c = 1;
+            }
+            else
+            {
+                c++;
+            }
+        }
+        s = t;
+    }
+    return s;
+}
+
+int main(){
+    cout<<countandsay(4)<<endl;
+}

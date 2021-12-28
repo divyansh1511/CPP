0a93c56d8da63cfa792c6a892aa0ca42e4865049
DIVYANSH GARG <dishugarg1511@gmail.com>
2021-12-28 18:15:05 +0530

    start

---
+++ DP GFG/easy/2_fibnocci_no.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+    int dp[n];
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i < n; i++)
+    {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n-1];
+}
+
+int main(){
+    cout<<fib(9)<<endl;
+}

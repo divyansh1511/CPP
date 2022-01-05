73af72e4eb5fb1f456d638f43f4ecb22aadfbf54
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-01-05 15:50:27 +0530

    d

---
+++ Striver sheet/DAY 3/5.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int uniquegrid(int m , int n){
+    // vector<vector<int>> arr(m, vector<int>(n, 1));
+    int arr[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                arr[i][j] = 1;
+            }
+            else{
+                arr[i][j] = arr[i-1][j] + arr[i][j-1];
+            }
+        }
+    }
+    return arr[m-1][n-1];
+}
+
+int main(){
+    cout<<uniquegrid(3 , 7)<<endl;
+}

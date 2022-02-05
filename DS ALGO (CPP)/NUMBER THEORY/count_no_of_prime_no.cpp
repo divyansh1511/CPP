1a4438369b9f46adcc50cd15ce1f31102cb6e29a
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-02-05 17:37:23 +0530

    REVISION DAY - 2

---
+++ DS ALGO (CPP)/NUMBER THEORY/count_no_of_prime_no.cpp
@@ -0,0 +1,30 @@
+//sieve of eratosthenes algorithm
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<bool> v(n+1 , true);
+    v[0] = false;
+    v[1] = false;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (v[i] == true)
+        {
+            for (int j = i*i; j <= n; j += i)
+            {
+                v[j] = false;
+            }
+        }
+    }
+    int count = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        if (v[i] == true)
+        {
+            count++;
+        }
+    }
+    cout<<count<<endl;
+}

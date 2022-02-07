9fa223663409a244dc24f1a9346e90ec519cbd54
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-02-07 21:45:23 +0530

    PLACEMENT DAY - 1

--- INTERVIEW BIT/ARRAYS/Pascals_triangle.cpp
+++ INTERVIEW BIT/ARRAYS/Pascals_triangle.cpp
@@ -6,9 +6,8 @@ vector<vector<int>> pascalstriangle(int n){
     for (int i = 0; i < n; i++)
     {
         v[i].resize(i+1);
-        v[i][0] = v[i][1] = 1;
-        for (int j = i; j < i; j++)
-        {
+        v[i][0] = v[i][i] = 1;
+        for(int j = 1;j<i;j++){
             v[i][j] = v[i-1][j-1] + v[i-1][j];
         }
     }

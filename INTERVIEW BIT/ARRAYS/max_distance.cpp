b25e4d274a62d9fc15c879227a2a5bf2d427c100
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-02-08 15:38:25 +0530

    PLACEMENT DAY - 2

---
+++ INTERVIEW BIT/ARRAYS/max_distance.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxgap(vector<int> A){
+    int n=A.size();
+    vector<pair<int,int> >v;
+    for(int i=0;i<n;i++){
+        v.push_back(make_pair(A[i],i));
+    }
+    sort(v.begin(),v.end());
+    int ans=0;
+    int rmax=v[n-1].second;
+    for(int i=n-2;i>=0;i--){
+        ans=max(ans,rmax-v[i].second);
+        rmax=max(rmax,v[i].second);
+    }
+    return ans;
+}
+
+int main(){
+    
+}

d491fe1a0e5d86c7c32b47023cc1ac1c62a4a294
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-02-09 12:09:59 +0530

    PLACEMENT DAY - 2

--- INTERVIEW BIT/ARRAYS/spiral_order_matrix.cpp
+++ INTERVIEW BIT/ARRAYS/spiral_order_matrix.cpp
@@ -2,44 +2,39 @@
 using namespace std;
 
 vector<vector<int>> spiralorder(int A){
-    vector<vector<int>> v(A , vector<int>(A));
-    int i, l = 0 , k = 0;
+    vector<vector<int>> a(A , vector<int>(A));
+    int i, k = 0, l = 0;
     int c = 1;
-    int m = A, n = A;
-    while (l < m && k < n)
-    {
-        for (i = 0; i < n; i++)
-        {
-            v[k][i] = c;
+    int m = A , n = A;
+    while (k < m && l < n) {
+        for (i = l; i < n; ++i) {
+            a[k][i] = c;
             c++;
         }
         k++;
-        for (i = k; i < m; i++)
-        {
-            v[i][n-1] = c;
+        for (i = k; i < m; ++i) {
+            a[i][n-1] = c;
             c++;
         }
         n--;
-        if (k < m)
-        {
-            for (i = n-1; i >= l; i--)
-            {
-                v[m-1][i] = c;
+
+        if (k < m) {
+            for (i = n - 1; i >= l; --i) {
+                a[m-1][i] = c;
                 c++;
             }
             m--;
         }
-        if (l < n)
-        {
-            for (i = m-1; i >= k; i--)
-            {
-                v[i][l] = c;
+
+        if (l < n) {
+            for (i = m - 1; i >= k; --i) {
+                a[i][l] = c;
                 c++;
             }
             l++;
         }
     }
-    return v;
+    return a;
 }
 
 int main(){

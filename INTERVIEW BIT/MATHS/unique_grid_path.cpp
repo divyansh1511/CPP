a2d89c1cb6b240ca8b95f6b5fd8e832dfd1c6c04
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-02-11 13:28:00 +0530

    PLACEMENT DAY - 5

--- INTERVIEW BIT/MATHS/unique_grid_path.cpp
+++ INTERVIEW BIT/MATHS/unique_grid_path.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//----------------RECURSTION-----------------------//
+
 int uniquePathhelper(int a , int b){
     if (a < 0 || b < 0)
     {
@@ -20,6 +22,21 @@ int uniquePath(int a , int b){
     return uniquePathhelper(a-1 , b-1);
 }
 
+// -------------------------- DP--------------------//
+
+int uniquepath(int a , int b){
+    vector<vector<int>> dp(a , vector<int>(b , 1));
+    for (int i = 1; i < a; i++)
+    {
+        for (int j = 1; j < b; j++)
+        {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+    return dp[a-1][b-1];
+}
+
 int main(){
     cout<<uniquePath(3 , 7)<<endl;
+    cout<<uniquepath(3 , 7)<<endl;
 }

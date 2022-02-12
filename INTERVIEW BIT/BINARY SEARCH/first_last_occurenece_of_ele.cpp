56031c4daf66796f80a98034b6c4d25ccb7528db
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-02-12 15:45:53 +0530

    PLACEMENT DAY - 6

--- INTERVIEW BIT/BINARY SEARCH/first_last_occurenece_of_ele.cpp
+++ INTERVIEW BIT/BINARY SEARCH/first_last_occurenece_of_ele.cpp
@@ -5,21 +5,46 @@ int firstoccurence(int arr[] , int n , int k){
     int i = 0 , j = n-1;
     int mid = i + (j-i)/2;
     int ans = 0;
-    while (i < j)
+    while (i <= j)
     {
         if (arr[mid] == k)
         {
             ans = mid;
             j = mid-1;
         }
-        if (arr[i] < k)
+        else if (arr[mid] < k)
         {
             i = mid+1;
         }
-        else
+        else if(arr[mid] > k)
         {
             j = mid-1;
         }
+        mid = i + (j-i)/2;
+    }
+    return ans;
+}
+
+int lastoccurence(int arr[] , int n , int k){
+    int i = 0 , j = n-1;
+    int mid = i + (j-i)/2;
+    int ans = 0;
+    while (i <= j)
+    {
+        if (arr[mid] == k)
+        {
+            ans = mid;
+            i = mid+1;
+        }
+        if (arr[mid] < k)
+        {
+            i = mid+1;
+        }
+        else if(arr[mid] > k)
+        {
+            j = mid-1;
+        }
+        mid = i + (j-i)/2;
     }
     return ans;
 }
@@ -27,4 +52,5 @@ int firstoccurence(int arr[] , int n , int k){
 int main(){
     int arr[] = {10,20,30,30,30,50};
     cout<<firstoccurence(arr , 6 , 30)<<endl;
+    cout<<lastoccurence(arr , 6 , 30)<<endl;
 }

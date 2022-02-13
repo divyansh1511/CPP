16e597fdf54545ce34c6933b089b7d002ae447cf
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-02-13 11:19:46 +0530

    PLACEMENT DAY - 6

--- INTERVIEW BIT/BINARY SEARCH/search_rotated_sorted_arr.cpp
+++ INTERVIEW BIT/BINARY SEARCH/search_rotated_sorted_arr.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//-------------------------------1st-----------------------//
+
 int findPivot(int arr[] , int n){
     int i = 0 , j = n-1;
     int mid = i + (j-i)/2;
@@ -47,6 +49,44 @@ int binarysearch(int arr[] , int n , int k , int s , int e){
     return -1;
 }
 
+//--------------------------------2nd-------------------------------//
+
+int searchrotated(int arr[] , int n , int k){
+    int i = 0 , j = n-1;
+    int mid = i + (j-i)/2;
+    while (i <= j)
+    {
+        if (arr[mid] == k)
+        {
+            return mid;
+        }
+        if (arr[0] <= arr[mid])
+        {
+            if (arr[mid] > k && k >= arr[0])
+            {
+                j = mid - 1;
+            }
+            else
+            {
+                i = mid+1;
+            }
+        }
+        else
+        {
+            if (arr[mid] < k && k <= arr[n-1])
+            {
+                i = mid+1;
+            }
+            else
+            {
+                j = mid - 1;
+            }
+        }
+        mid = i + (j-i)/2;
+    }
+    return -1;
+}
+
 int main(){
     int arr[] = {5,6,7,1,2,3,4};
     int pivot = findPivot(arr , 7);
@@ -59,5 +99,5 @@ int main(){
     {
         cout<<binarysearch(arr , 7 , 3 , pivot , 6)<<endl;
     }
-    
+    cout<<searchrotated(arr , 7 , 3)<<endl;
 }

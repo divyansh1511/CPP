7fc01906ce463ed69b33ed8169a7d57f0e593599
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-03-28 17:47:25 +0530

    pp

---
+++ LEETCODE/Dynamic programing/esay/5.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int trinomi(int n){
+    int dp[n+1];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    }
+    return dp[n];
+}
+
+int main(){
+    cout<<trinomi(7)<<endl;
+}

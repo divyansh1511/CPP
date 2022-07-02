eaa86e56427007d41ddd3fb1f09a436922dc4315
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-07-02 16:49:09 +0530

    correction

--- COMPLETE TOPICS/1_Complete_Arrays.cpp
+++ COMPLETE TOPICS/1_Complete_Arrays.cpp
@@ -177,7 +177,7 @@ int maxsubarrsum(int arr[] , int n){
 int minimize_heights(int arr[] , int n , int k){
     sort(arr , arr+n);
     int diff = arr[n-1] - arr[0];
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
         int maxi = max(arr[i-1]+k , arr[n-1]-k);
         int mini = min(arr[0]+k , arr[i]-k);

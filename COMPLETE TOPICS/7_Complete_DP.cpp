988b958632fd25cb974b470fb94dce62692d93ff
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-09-05 15:40:11 +0530

    up

--- COMPLETE TOPICS/7_Complete_DP.cpp
+++ COMPLETE TOPICS/7_Complete_DP.cpp
@@ -252,11 +252,11 @@ int coinchange_minnoofcoins(int arr[] , int n , int sum){
     int dp[n+1][sum+1];
     for (int i = 0; i <= n; i++)
     {
-        dp[i][0] = 1;
+        dp[i][0] = 0;
     }
     for (int i = 0; i <= sum; i++)
     {
-        dp[0][i] = 0;
+        dp[0][i] = INT_MAX-1;
     }
     for (int i = 1; i <= n; i++)
     {
@@ -264,7 +264,7 @@ int coinchange_minnoofcoins(int arr[] , int n , int sum){
         {
             if (arr[i-1] <= j)
             {
-                dp[i][j] = min(dp[i][j-arr[i-1]] , dp[i-1][j]);
+                dp[i][j] = min(dp[i][j-arr[i-1]]+1 , dp[i-1][j]);
             }
             else
             {
